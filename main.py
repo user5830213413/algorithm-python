d8416758fd3c01bc5cfd43134247d19b6f0d56fa
hattabich <hattabich@gmail.com>
2023-07-30 12:52:53 +0300

    bubble method algorithm

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ def sort(array):
 
 
 print(sort(array=[5,9,4,6,2,1]))
+
+#пузырьковый алгоритм сортировки
+def bubble_method(array_bubble, count: int = 0):
+    n = len(array_bubble)
+    for r in range(n - 1):
+        for i in range(n - 1):
+            if array_bubble[i]>array_bubble[i + 1]:
+                count += 1
+                array_bubble[i],array_bubble[i + 1] = array_bubble[i+1], array_bubble[i]
+
+    return array_bubble, count
+
+
+print(bubble_method([5,7,4,3,8,2]))
