41f9eba7eee9ce51fa8eee6f1cdbde0593733b91
hattabich <hattabich@gmail.com>
2023-08-05 00:21:55 +0300

    алгоритм чисел

--- numbers_1.py
+++ numbers_1.py
@@ -4,3 +4,16 @@ test_1 = (num // 10 ** 2) % 10 # вторая цифра
 test_2 = (num // 10) % 10 # предпоследняя
 test_3 = num % 10 # последняя
 print(f'{first_digit}\n{test_1}\n{test_2}\n{test_3}')
+
+# разложение на множетили, x = целое число
+def simple(x):
+    divisor = 2
+    test = []
+    while x > 1:
+        if x % divisor == 0:
+            test.append(divisor)
+            x //= divisor
+        else:
+            divisor += 1
+    return test
+print(simple(56))
